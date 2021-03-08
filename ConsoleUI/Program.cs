fad54c42d969610b6f77f46a1f8932cd1f7d171b
Ahmet Şengül <74301219+Ahmet-Sengul@users.noreply.github.com>
2021-03-08 17:04:10 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,47 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductTest();
+            //CotegoryTest();
+
+        }
+
+        private static void CotegoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll().Data)
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal(), new CategoryManager(new EfCategoryDal()));
+
+            var result = productManager.GetProcutDetails();
+
+            if (result.Success)
+            {
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+
+        }
+    }
+}
